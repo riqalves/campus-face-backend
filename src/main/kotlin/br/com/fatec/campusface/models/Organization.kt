0f022c67b784cf3a7a1941b1f6d44a94de457a2f
riqalves <henrique12095@gmail.com>
2026-01-07 15:55:16 -0300

    refactor: updating organization to sql

--- src/main/kotlin/br/com/fatec/campusface/models/Organization.kt
+++ src/main/kotlin/br/com/fatec/campusface/models/Organization.kt
@@ -1,16 +1,20 @@
 package br.com.fatec.campusface.models
 
+import jakarta.persistence.*
 import java.time.Instant
 
-
+@Entity
+@Table(name = "organizations")
 data class Organization(
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     val id: String = "",
+    @Column(nullable = false)
     val name: String = "",
     val description: String = "",
+
+    @Column(name = "hub_code", nullable = false, unique = true)
     val hubCode: String = "",
-    val adminIds: List<String> = emptyList(),
-    val validatorIds: List<String> = emptyList(),
-    val memberIds: List<String> = emptyList(),
     val createdAt: Instant = Instant.now(),
     val updatedAt: Instant = Instant.now()
 )
